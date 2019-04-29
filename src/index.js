29016e726c90bf5913955476a266c7ad409dbb5f
Jascha <jascha@jaeh.at>
2019-04-29 04:54:17 +0200

    add src and test

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const tryCatch = fn => (...args) => {
+  try {
+    const res = fn(...args)
+    if (res === '[]' && Object.keys(args[0]).length) {
+      throw new Error('Serialization error')
+    }
+    return res
+  } catch(e) {
+    return e
+  }
+}
+
+module.exports = {
+  parse: tryCatch(JSON.parse),
+  stringify: tryCatch(JSON.stringify),
+}
