2e53d20658d81e56ea41218e10fd2b9dd4795e8b
jascha <jascha@jaeh.at>
2020-03-16 08:27:43 +0100

    index: remove tryCatch import

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,2 @@
-// the import and const lib = statements are only here for testing.
-import tryCatch from '@magic-libraries/try-catch'
-
-const lib = {
-  tryCatch,
-}
-
 export const parse = lib.tryCatch(JSON.parse)
 export const stringify = lib.tryCatch(JSON.stringify)
-
-export default {
-  parse,
-  stringify,
-}
